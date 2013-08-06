a72f439ff3c777325b2887de1f284f608aae9fcc
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2013-08-06 12:15:16 +0200

    Completed translation into python

--- src/SLURMInfoUtils/SControlInfoHandler.py
+++ src/SLURMInfoUtils/SControlInfoHandler.py
@@ -19,9 +19,235 @@ import re
 import time
 import shlex
 import subprocess
+from threading import Thread
+
+
+class ErrorHandler(Thread):
+
+    def __init__(self, err_stream):
+        Thread.__init__(self)
+        self.stream = err_stream
+        self.message = ""
+    
+    def run(self):
+        line = self.stream.readline()
+        while line:
+            self.message = self.message + line
+            line = self.stream.readline()
+
+
+class NodesInfoHandler(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.ncpu = 0
+        self.nfree = 0
+        self.stateRegex = re.compile('State=(IDLE|COMPLETING|ALLOCATED|DRAINING|MIXED)')
+        self.tcpuRegex = re.compile('CPUTot=([0-9]+)')
+        self.acpuRegex = re.compile('CPUAlloc=([0-9]+)')
+    
+    def setStream(self, stream):
+        self.stream = stream
+    
+    
+    def run(self):
+        line = self.stream.readline()
+        
+        while line:
+
+            try:
+                parsed = self.stateRegex.search(line)
+                if not parsed:
+                    continue
+                
+                nodeState = parsed.group(1)
+                
+                parsed = self.tcpuRegex.search(line)
+                if not parsed:
+                    continue
+            
+                tcpu = int(parsed.group(1))
+            
+                parsed = self.acpuRegex.search(line)
+                if not parsed:
+                    continue
+            
+                acpu = int(parsed.group(1))
+
+                self.ncpu += acpu
+                if nodeState <> "DRAINING":
+                    self.nfree += (tcpu - acpu)
+
+            finally:
+                line = self.stream.readline()
+
+
+
+class JobInfoHandler(Thread):
+
+    def __init__(self, oStream):
+        Thread.__init__(self)
+        self.outStream = oStream
+        
+        self.jidRegex = re.compile('JobId=([^\s]+)')
+        self.nameRegex = re.compile('Name=([^\s]+)')
+        self.uidRegex = re.compile('UserId=([^(]+)')
+        self.gidRegex = re.compile('GroupId=([^(]+)')
+        self.jstateRegex = re.compile('JobState=(RUNNING|PENDING)')
+        self.tlimitRegex = re.compile('TimeLimit=(UNLIMITED|[0-9:-]+)')
+        self.subtimeRegex = re.compile('SubmitTime=([^\s]+)')
+        self.sttimeRegex = re.compile('StartTime=([^\s]+)')
+        self.partRegex = re.compile('Partition=([^\s]+)')
+        self.ncpuRegex = re.compile('NumCPUs=([0-9]+)')
+
+    def setStream(self, stream):
+        self.stream = stream
+    
+
+    def convertTLimit(self, tstr):
+        if tstr == "UNLIMITED":
+            return None
+        tmpl = tstr.split('-')
+        if len(tmpl) > 1:
+            result = int(tmpl[0]) * 86400
+            hStr = tmpl[1]
+        else:
+            result = 0
+            hStr = tmpl[0]
+        
+        tmpl = hStr.split(':')
+        if len(tmpl) > 0:
+            result += int(tmpl[0]) * 3600
+        if len(tmpl) > 1:
+            result += int(tmpl[1]) * 60
+        if len(tmpl) > 2:
+            result += int(tmpl[2])
+            
+        return result
+                
+        
+    def run(self):
+        line = self.stream.readline()
+        
+        while line:
+        
+            try:
+            
+                parsed = self.jstateRegex.search(line)
+                if not parsed:
+                    continue
+                jState = parsed.group(1)
+                
+                parsed = self.jidRegex.search(line)
+                if not parsed:
+                    continue
+                jobId = parsed.group(1)
+                
+                parsed = self.nameRegex.search(line)
+                if not parsed:
+                    continue
+                name = parsed.group(1)
+                
+                parsed = self.uidRegex.search(line)
+                if not parsed:
+                    continue
+                uid = parsed.group(1)
+                
+                parsed = self.gidRegex.search(line)
+                if not parsed:
+                    continue
+                gid = parsed.group(1)
+                
+                parsed = self.partRegex.search(line)
+                if not parsed:
+                    continue
+                queue = parsed.group(1)
+                
+                parsed = self.ncpuRegex.search(line)
+                if not parsed:
+                    continue
+                cpuCount = int(parsed.group(1))
+                
+                parsed = self.tlimitRegex.search(line)
+                if not parsed:
+                    timeLimit = None
+                else:
+                    timeLimit = self.convertTLimit(parsed.group(1))
+                
+                parsed = self.subtimeRegex.search(line)
+                if not parsed:
+                    continue
+                subTime = int(time.mktime(time.strptime(parsed.group(1), '%Y-%m-%dT%H:%M:%S')))
+                
+                parsed = self.sttimeRegex.search(line)
+                if not parsed:
+                    continue
+                startTime = int(time.mktime(time.strptime(parsed.group(1), '%Y-%m-%dT%H:%M:%S')))
+                
+                formatStr = "{'group': '%s', 'name': '%s', 'qtime': %d, 'jobid': '%s', 'queue': '%s', 'cpucount': %d, 'user': '%s', "
+                tmpbuff = formatStr % (gid, name, subTime, jobId, queue, cpuCount, uid)
+                
+                #
+                # timeLimit == None means UNLIMITED
+                #
+                if timeLimit:
+                    tmpbuff += "'maxwalltime': %d, " % timeLimit
+                    
+                if jState == "RUNNING":
+                    tmpbuff += "'start': %d, 'state': 'running'}\n" % startTime
+                else:
+                    tmpbuff += "'state': 'queued'}\n"
+                
+                self.outStream.write(tmpbuff)
+                
+            finally:
+                line = self.stream.readline()
 
 
 def parseCPUInfo(filename=None):
-    pass
+    if filename:
+        cmd = shlex.split('cat ' + filename)
+    else:
+        cmd = shlex.split('scontrol -o show nodes')
+    
+    container = NodesInfoHandler()
+    parseStream(cmd, container)
+    return container.ncpu, container.nfree
+
+
+def parseJobInfo(outStream, filename=None):
+    if filename:
+        cmd = shlex.split('cat ' + filename)
+    else:
+        cmd = shlex.split('scontrol -o show jobs')
+    
+    container JobInfoHandler(outStream)
+    parseStream(cmd, container)
+
+
+def parseStream(cmd, container)
+    try:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+        stdout_thread = container.setStream(process.stdout)
+        stderr_thread = ErrorHandler(process.stderr)
+    
+        stdout_thread.start()
+        stderr_thread.start()
+    
+        ret_code = process.wait()
+    
+        stdout_thread.join()
+        stderr_thread.join()
+        
+        if ret_code <> 0:
+            raise Exception(stderr_thread.message)
+            
+        if len(stdout_thread.errList) > 0:
+            raise Exception(stdout_thread.errList[0])
+
+    except:
+        etype, evalue, etraceback = sys.exc_info()
+        raise Exception("%s: (%s)" % (etype, evalue))
     
 
